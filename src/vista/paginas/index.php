4803d490115e16ee98849b8492b31076b0b83592
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-10-05 13:37:49 -0300

    agregando el tutorial de las 2 librerias

--- src/vista/paginas/index.php
+++ src/vista/paginas/index.php
@@ -3,8 +3,25 @@
 include_once '../estructura/header.php';
 ?>
 <div class="contenedorPrincipal"> 
-    <h1>PAGINA DE INICIO</h1>
-    <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio qui ipsa fugiat hic, voluptatibus soluta et fuga, accusamus expedita ut a reiciendis non debitis modi recusandae, quia sit asperiores? Nisi.</div>
+    <div class="contenido-descarga container-sm mt-4">
+        <p>mpdf/qrcode (PHP Edition)</p>
+        <a href="#">Descarga Qrcode</a>
+    </div>
+    <div class="container-sm mt-4 ">
+        <h1>Instalacion de Qrcode</h1>
+    </div>
+    <div class="contenido-compatibilidades container-sm mt-4">
+        <h2>compatibilidad de version de PHP</h2>
+    </div>
+    <div class="container-sm mt-4">
+        <h2>Extraccion de la distribucion de Qrcode</h2>
+        <p>Qrcode para PHP es compatible con Windows , linux y macOS .Verifique que la distribucion de Qrcode que ha descargado sea para la edicion del sistema operativo de PHP que esta usando .Si no esta seguro del tipo de PHP que esta usando .Puede usar phpInfo para averiguarlo</p>
+        <p>Para comenzar , puede usar <a href="https://getcomposer.org/">Composer</a> para poder instalar la libreria mas facilmente</p>
+    </div>
+    <div class="container-sm mt-4">
+        <h2>Instalacion de Qrcode para PHP</h2>
+    </div>
+    
 </div>
 
 <?php
