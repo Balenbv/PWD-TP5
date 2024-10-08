5907c597d6ed5467c9f5f1d73ffa61586e1cd5ec
DiegoAvila1 <diegoavilaaa0@gmail.com>
2024-10-08 14:10:26 -0300

    layout de manejo de errores

--- src/vista/action/accionManejo.php
+++ src/vista/action/accionManejo.php
@@ -2,6 +2,7 @@
 require '../../../config.php';
 require '../../../vendor/autoload.php';
 include '../estructura/header.php';
+
 $datos = data_submitted();
 $abm = new abmPersona();
 
@@ -14,68 +15,91 @@ $whoops->register();
 ?>
 
 <div class="contenedorPrincipal">
+    <div class="container tex-center">
     <h1>Manejo de errores por Whoops</h1>
+    </div>
 
-
-    <h2>Error 1:</h2>
-        <h3>Error Fatal</h3>
-        <code>
-            <pre>
-            $a = 10;
-            $b = 0;
-            function division($a, $b){
-                return $a / $b;
-            }
-            division($a, $b);
-            </pre>
-        </code>
-    <button onclick="moverA(1)">Ver Error</button>
-
-    <h2>Error 2</h2>
-        <h3>Error de Parseo</h3>
-        <code>
-            <pre>
-            eval('echo "Esto es un error de parseo;');
-            </pre>
-        </code>
-    <button onclick="moverA(2)"> Ver Error </button>
-
-    <h2>Error 3</h2>
-        <h3>Error de tiempo de Ejecucion</h3>
-        <code>
-            <pre>
-            $array = [1, 2, 3];
-            echo $array[5];
-            </pre>
-        </code>
-    <button onclick="moverA(3)">Ver Error</button>
-
-    <h2>Error 4</h2>
-        <h3>Excepcion no capturada</h3>
-        <code>
-            <pre>
-            throw new Exception("Esto es una excepción no capturada");
-            </pre>
-        </code>
-    <button onclick="moverA(4)">Ver Error</button>
-
-    <h2>Error 5</h2>
-        <h3>Error de Notificacion</h3>
-        <code>
-            <pre>
-            echo $variable_no_definida;
-            </pre>
-        </code>
-    <button onclick="moverA(5)">Ver Error</button>
-    
-    <h2>Error 6</h2>
-        <h3>Error de Usuario</h3>
-        <code>
-            <pre>
-            trigger_error("Esto es un error de usuario", E_USER_NOTICE);
-            </pre>
-        </code>
-    <button onclick="moverA(6)">Ver Error</button>
+    <div class="grid-container">
+        <div class="item-grid">
+            <h2>Error 1:</h2>
+                <h2>Error Fatal</h2>
+                <code><pre>
+$a = 10;
+$b = 0;
+function division($a, $b){
+    return $a / $b;
+}
+division($a, $b);
+                    </pre></code>
+                    <div class="container-btn">
+                     <button onclick="moverA(1)" class="flex-btn">Ver Error</button>
+                    </div>
+        </div>
+        <div class="item-grid">
+            <h2>Error 2</h2>
+                <h2>Error de Parseo</h2>
+                <code>
+                    <pre>
+eval('echo "Esto es un error de parseo;');
+                    </pre>
+                </code> 
+                <div class="container-btn btn-2">
+                    <button onclick="moverA(2)" class="flex-btn"> Ver Error </button>
+                </div>
+            
+        </div>
+        <div class="item-grid">
+            <h2>Error 3</h2>
+                <h2>Error de tiempo de Ejecucion</h2>
+                <code>
+                    <pre>
+$array = [1, 2, 3];
+echo $array[5];
+                    </pre>
+                </code>
+                <div class="container-btn btn-3">
+                    <button onclick="moverA(3)" class="flex-btn">Ver Error</button>
+                </div>
+            
+        </div>
+        <div class="item-grid">
+            <h2>Error 4</h2>
+                <h2>Excepcion no capturada</h2>
+                <code>
+                    <pre>
+throw new Exception("Esto es una excepción no capturada");
+                    </pre>
+                </code>
+                <div class="container-btn mt-5">
+                    <button onclick="moverA(4)" class="flex-btn">Ver Error</button>
+                </div>
+            
+        </div>
+        <div class="item-grid">
+            <h2>Error 5</h2>
+                <h2>Error de Notificacion</h2>
+                <code>
+                    <pre>
+echo $variable_no_definida;
+                    </pre>
+                </code>
+                <div class="container-btn mt-5 pt-3">
+                    <button onclick="moverA(5)" class="flex-btn">Ver Error</button>
+                </div>
+        </div>
+        <div class="item-grid">
+            <h2>Error 6</h2>
+                <h2>Error de Usuario</h2>
+                <code>
+                    <pre>
+trigger_error("Esto es un error de usuario",E_USER_NOTICE);
+                    </pre>
+                </code>
+                <div class="container-btn mt-5">
+                    <button onclick="moverA(6)" class="flex-btn">Ver Error</button>
+                </div>
+        </div>
+    </div>
 </div>
 
 <script>
